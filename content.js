89246e6ffabb18ae19b04201282ead5b25609ba6
Mast Luo <65349-mast_luo@users.noreply.drupalcode.org>
2025-03-12 15:59:59 +0800

    update

--- content.js
+++ content.js
@@ -1,6 +1,22 @@
 // 初始化屏蔽列表和观察器
 let blockedUsers = new Map();
 const observer = new MutationObserver(checkNewContent);
+let currentUserName = null;
+
+// 查询当前登录用户
+function findCurrentUser() {
+    document.querySelectorAll("ul#sidebar-section-content-community > li.sidebar-section-link-wrapper").forEach(element => {
+        const itemName = element.attributes.getNamedItem("data-list-item-name");
+        if (itemName && itemName.value === "my-posts") {
+            const activeUrl = element.querySelector("a").href;
+            currentUserName = activeUrl.split("/").slice(-2, -1)[0];
+
+            return currentUserName;
+        }
+    });
+
+    return null;
+}
 
 // 从存储加载已屏蔽用户
 chrome.storage.local.get('blockedUsers', (data) => {
@@ -10,9 +26,30 @@ chrome.storage.local.get('blockedUsers', (data) => {
 
 // 添加屏蔽按钮
 function addBlockButtons() {
+    let authorId = null;
+    let authorName = null;
+    let authorAvatar = null;
+
+    if (!currentUserName) {
+        currentUserName = findCurrentUser();
+    }
+
     const currentUrl = window.location.href;
     if (currentUrl.startsWith("https://linux.do/t/topic/")) {
         const postAuthor = document.querySelector("article#post_1.boxed.onscreen-post");
+        if (postAuthor) {
+            authorId = postAuthor.attributes.getNamedItem("data-user-id").value;
+            const authorNameEle = postAuthor.querySelector(".row > .topic-avatar > .post-avatar > a")
+            authorName = authorNameEle ? authorNameEle.attributes.getNamedItem("data-user-card").value : null;
+
+            // 如果是自己的帖子，则不添加屏蔽按钮
+            if (currentUserName && authorName === currentUserName) {
+                return;
+            }
+
+            const authorAvatarEle = authorNameEle.querySelector("img");
+            authorAvatar = authorAvatarEle ? "https://linux.do" + authorAvatarEle.attributes.getNamedItem("src").value : null;
+        }
 
         // 查找是否已经存在屏蔽按钮容器
         let blockButtonContainer = document.querySelector("#post_1 > div.topic-map.--op > section > div > div.topic-map__buttons >div.block-user-button");
@@ -31,32 +68,72 @@ function addBlockButtons() {
             blockButton.className = "btn btn-default top-replies";
             blockButton.title = "屏蔽该用户";
             blockButton.type = "button";
+            blockButton.dataset.id = authorId || null;
+            blockButton.dataset.avatar = authorAvatar || null;
+            blockButton.dataset.name = authorName || null;
             blockButton.appendChild(buttonText);
-            blockButton.addEventListener('click', ()=>{console.log("clicked")});
+            blockButton.addEventListener('click', handleBlockClick);
 
             // 将按钮添加到容器中，再将容器添加到页面上
             blockButtonContainer.appendChild(blockButton);
             document.querySelector("#post_1 > div.topic-map.--op > section > div > div.topic-map__buttons").appendChild(blockButtonContainer);
         }
-        if (postAuthor) {
-            const author_id = postAuthor.attributes.getNamedItem("data-user-id").value;
+
+        // 更新屏蔽按钮状态
+        const blockButton = blockButtonContainer.querySelector("button");
+        if (blockedUsers.has(authorName)) {
+            blockButton.querySelector("span").textContent = "取消屏蔽";
+            blockButton.title = "取消屏蔽该用户";
+            blockButton.addEventListener('click', handleUnBlockClick)
         }
     }
 }
 
 // 屏蔽点击处理
 function handleBlockClick(event) {
-    const username = event.target.dataset.username;
-    blockedUsers.add(username);
-    chrome.storage.local.set({ blockedUsers: [...blockedUsers] });
-    applyBlocking();
+    const userId = event.currentTarget.dataset.id;
+    const userAvatar = event.currentTarget.dataset.avatar;
+    const userName = event.currentTarget.dataset.name;
+    if (userId && userAvatar && userName) {
+        if (!blockedUsers.has(userName)) {
+            blockedUsers.set(userName, { id: userId, avatar: userAvatar, name: userName });
+            chrome.storage.local.set({ blockedUsers: [...blockedUsers] });
+            applyBlocking();
+            event.target.textContent = "取消屏蔽";
+        }
+    }
+}
+
+// 取消屏蔽点击处理
+function handleUnBlockClick(event) {
+    const userName = event.currentTarget.dataset.name;
+    if (userName) {
+        blockedUsers.delete(userName);
+        chrome.storage.local.set({ blockedUsers: [...blockedUsers] });
+        applyBlocking();
+        event.target.textContent = "屏蔽";
+    }
 }
 
 // 应用屏蔽规则
 function applyBlocking() {
-    document.querySelectorAll('.post, .comment').forEach(post => {
-        const username = post.querySelector('.username').textContent.trim();
-        post.style.display = blockedUsers.has(username) ? 'none' : '';
+    // 隐藏已屏蔽用户的帖子
+    document.querySelectorAll("tbody.topic-list-body > tr").forEach(element => {
+        const postAuthor = element.querySelector("td.posters.topic-list-data > a")
+        const userName = postAuthor ? postAuthor.attributes.getNamedItem("data-user-card").value : null;
+        if (blockedUsers.has(userName)) {
+            element.style.display = "none";
+        }
+    });
+
+    // 隐藏已屏蔽用户的评论
+    document.querySelectorAll("article").forEach(element => {
+        const userName = element.querySelector(".row > .topic-avatar > .post-avatar > a").attributes.getNamedItem("data-user-card").value;
+        if (blockedUsers.has(userName)) {
+            if (element.id !== "post_1") {
+                element.parentElement.style.display = "none";
+            }
+        }
     });
 }
 
@@ -67,7 +144,7 @@ function checkNewContent(mutations) {
     debunceTimeout = setTimeout(() => {
         addBlockButtons();
         applyBlocking();
-    }, 300);
+    }, 2000);
 }
 
 // 启动观察器
@@ -79,5 +156,6 @@ observer.observe(document.body, {
 });
 
 // 初始化执行
+findCurrentUser();
 addBlockButtons();
 applyBlocking();
